d7dd3b372d954ec0868f8c788670093dbd582e3b
cda <rkcdacda@gmail.com>
2020-08-08 15:18:50 +0200

    debugging build

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
-git clone --branch v2.0.0 https://github.com/coreos/go-etcd /src/github.com/coreos/go-etcd
-git clone -b swarm-mode https://github.com/marcuslinke/registrator.git /src/github.com/marcuslinke/registrator
-cd /src/github.com/markuslinke/registrator
-go get -d -v ./...
-go build -ldflags "-X main.Version=swarm-mode" .
-cp registrator /drone/src/rpi-registrator/
-export IMAGE_DIR=/drone/src/buildresult/docker-images/${PACKAGE_NAME}/$(date +"%Y-%m-%d_%H%M")_$(git rev-parse --short HEAD)  && 
-mkdir -p ${IMAGE_DIR} &&
-ln -s ${IMAGE_DIR} /drone/src/buildresult/latest
+# git clone --branch v2.0.0 https://github.com/coreos/go-etcd /src/github.com/coreos/go-etcd
+# git clone -b swarm-mode https://github.com/marcuslinke/registrator.git /src/github.com/marcuslinke/registrator
+# cd /src/github.com/markuslinke/registrator
+# go get -d -v ./...
+# go build -ldflags "-X main.Version=swarm-mode" .
+# cp registrator /drone/src/rpi-registrator/
+# export IMAGE_DIR=/drone/src/buildresult/docker-images/${PACKAGE_NAME}/$(date +"%Y-%m-%d_%H%M")_$(git rev-parse --short HEAD)  && 
+# mkdir -p ${IMAGE_DIR} &&
+# ln -s ${IMAGE_DIR} /drone/src/buildresult/latest
